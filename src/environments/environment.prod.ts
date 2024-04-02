f90aeffb2d8bd13b1b458e11f9d00ce6840a096f
bassarou yacoubou <yacouboubassarou@gmail.com>
2024-04-02 07:26:05 +0100

    corriger env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,4 +5,5 @@ export const environment = {
   ssoUrlCallback: 'https://tourisme-app.star-labs.bj/admin/sso_callback',
   // ssoUrlCallback: "https://pprodofficial.service-public.bj/official/login?client_id=mtcalicence&redirect_uri=http://tourisme-app.star-labs.bj/admin/sso_callback&response_type=code&scope=openid&authError=true",
   expirationTime: 0,
+  default_statut_id_for_demands: 1,
 };
