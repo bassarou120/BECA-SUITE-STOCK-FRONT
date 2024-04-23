71498b482ef97572c1b8977a618cf3d960b0692d
MassitOps <massitahissou@gmail.com>
2024-04-23 12:17:43 +0100

    roles set with manual sidebar reload issue

--- src/app/core/services/data/data.service.ts
+++ src/app/core/services/data/data.service.ts
@@ -1,3 +1,4 @@
+import { AuthService } from 'src/app/core/core.index';
 import { Injectable } from '@angular/core';
 import { Observable, BehaviorSubject, map } from 'rxjs';
 import {
@@ -13,9 +14,9 @@ import { HttpClient } from '@angular/common/http';
 export class DataService {
   allAppliedCandidates!: Array<object>;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
-  public sideBar: SideBar[] = [
+  public grhSideBar: SideBar[] = [
     {
       tittle: 'Menu principal',
       icon: 'airplay',
@@ -43,8 +44,6 @@ export class DataService {
             },
           ],
         },
-
-
       ],
     },
     {
@@ -76,7 +75,6 @@ export class DataService {
 
           ],
         },
-
         {
           menuValue: 'Employés (GRH)',
           route: routes.employees,
@@ -141,7 +139,6 @@ export class DataService {
             },
           ],
         },
-
       ],
     },
     {
@@ -150,7 +147,6 @@ export class DataService {
       showAsTab: false,
       separateRoute: false,
       menu: [
-
         {
           menuValue: 'Fiche de paie',
           route: routes.payroll,
@@ -165,14 +161,10 @@ export class DataService {
               route: routes.fichePaie,
               base: 'employee-salary',
             },
-
           ],
         },
-
-
       ],
     },
-
     {
       tittle: 'PARAMETRAGE',
       icon: 'set',
@@ -250,8 +242,6 @@ export class DataService {
               base: 'type-prime',
             },
 
-
-
             // {
             //   menuValue: 'Statut',
             //   route: routes.statut,
@@ -260,13 +250,72 @@ export class DataService {
 
           ],
         },
-
-
       ],
     },
-
-
   ];
+
+  public employeSideBar: SideBar[] = [
+    {
+      tittle: 'Menu principal',
+      icon: 'airplay',
+      showAsTab: true,
+      separateRoute: false,
+      menu: [
+        {
+          menuValue: 'Tableau de bord',
+          route: routes.dashboard,
+          hasSubRoute: true,
+          showSubRoute: false,
+          icon: 'dashboard',
+          base: 'dashboard',
+          materialicons: 'home',
+          subMenus: [
+            {
+              menuValue: 'Employé Dashboard',
+              route: routes.employee,
+              base: 'employee',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      tittle: 'Gestion des Employés',
+      icon: 'layers',
+      showAsTab: false,
+      separateRoute: false,
+      menu: [
+        {
+          menuValue: 'Employés',
+          route: routes.employees,
+          hasSubRoute: true,
+          showSubRoute: false,
+          icon: 'user',
+          base: 'employees',
+          dot: true,
+          materialicons: 'people',
+          subMenus: [
+            {
+              menuValue: 'Mes Demandes',
+              route: routes.demandes,
+              base: 'demandes',
+            },
+            {
+              menuValue: 'Mes Plaintes',
+              route: routes.plaintes,
+              base: 'mes-plaintes',
+            },
+
+          ],
+        },
+      ],
+    },
+  ];
+
+  public sideBar = (this.authService.userRole && this.authService.userRole < 3) ? this.grhSideBar : this.employeSideBar;
+
+
+
   public getSideBarData: BehaviorSubject<Array<SideBar>> = new BehaviorSubject<
     Array<SideBar>
   >(this.sideBar);
