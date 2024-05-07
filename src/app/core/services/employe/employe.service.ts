22eddc1be2497d3a9eed689cc522962abc2f5111
bassarou yacoubou <yacouboubassarou@gmail.com>
2024-05-07 20:09:57 +0100

    correction   save my work

--- src/app/core/services/employe/employe.service.ts
+++ src/app/core/services/employe/employe.service.ts
@@ -48,6 +48,10 @@ export class EmployeService {
     return this.http.get<any>(`${this.url}/employe/${id}`);
   }
 
+  getlisteBanque(): Observable<any> {
+    return this.http.get<any>(`${this.url}/banques`);
+  }
+
   saveContatEmploye(data: any): Observable<Object> {
     return this.http.post(`${this.url}/contrats`, data);
   }
