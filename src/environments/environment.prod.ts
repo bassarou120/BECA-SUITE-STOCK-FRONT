05e2a689331b6f27866498035f7c60cc38b606d0
bassarou yacoubou <yacouboubassarou@gmail.com>
2024-06-10 17:48:40 +0100

    correction   save my work

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   backend: 'https://beca-suite-grh-api.beca-test.1020technologies.bj/api',
+  base_url_backend: 'https://beca-suite-grh-api.beca-test.1020technologies.bj/api',
 
   ssoUrlCallback: 'https://tourisme-app.star-labs.bj/admin/sso_callback',
   // ssoUrlCallback: "https://pprodofficial.service-public.bj/official/login?client_id=mtcalicence&redirect_uri=http://tourisme-app.star-labs.bj/admin/sso_callback&response_type=code&scope=openid&authError=true",
