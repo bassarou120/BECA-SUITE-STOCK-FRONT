f7606143d59ece93d5afe410468f8f9a018c6d31
bassarou yacoubou <yacouboubassarou@gmail.com>
2024-07-16 15:58:38 +0100

    en

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,8 @@
 export const environment = {
   production: true,
-  backend: 'https://beca-suite-grh-api.beca-test.1020technologies.bj/api',
-  base_url_backend: 'https://beca-suite-grh-api.beca-test.1020technologies.bj/api',
-
-  ssoUrlCallback: 'https://tourisme-app.star-labs.bj/admin/sso_callback',
-  // ssoUrlCallback: "https://pprodofficial.service-public.bj/official/login?client_id=mtcalicence&redirect_uri=http://tourisme-app.star-labs.bj/admin/sso_callback&response_type=code&scope=openid&authError=true",
-  expirationTime: 0,
+  backend: 'https://beca-backend-api-5azj.onrender.com/api',
+  base_url_backend: 'https://beca-backend-api-5azj.onrender.com/api',
+    expirationTime: 0,
   default_statut_id_for_demands: 1,
   default_statut_for_demands: "Non Traité",
 
