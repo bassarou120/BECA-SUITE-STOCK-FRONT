316528ea1c9a7ce4f51be8904345507e77c16907
bassarou yacoubou <yacouboubassarou@gmail.com>
2024-08-14 08:09:49 +0100

    gestion tableau de board

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  backend: 'https://beca-backend-api-5azj.onrender.com/api',
-  base_url_backend: 'https://beca-backend-api-5azj.onrender.com/api',
+  backend: 'https://beca-suite-api.topticsolutions.com/api',
+  base_url_backend: 'https://beca-suite-api.topticsolutions.com/api',
     expirationTime: 0,
   default_statut_id_for_demands: 1,
   default_statut_for_demands: "Non Traité",
