26bd68fe48932766b713fd47e60db51b7191c858
Bassarou yacoubou <yacouboubassarou@gmail.com>
2026-02-04 14:24:27 +0100

    tet

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,15 @@
 export const environment = {
+  // production: true,
+  // backend: 'https://beca-suite-api.topticsolutions.com/api',
+  // base_url_backend: 'https://beca-suite-api.topticsolutions.com/api',
+  //   expirationTime: 0,
+  // default_statut_id_for_demands: 1,
+  // default_statut_for_demands: "Non Traité",
+
   production: true,
-  backend: 'https://beca-suite-api.topticsolutions.com/api',
-  base_url_backend: 'https://beca-suite-api.topticsolutions.com/api',
-    expirationTime: 0,
+  backend: 'http://192.168.2.18:8081/api',
+  base_url_backend: 'http://192.168.2.18:8081',
+  expirationTime: 0,
   default_statut_id_for_demands: 1,
   default_statut_for_demands: "Non Traité",
 
