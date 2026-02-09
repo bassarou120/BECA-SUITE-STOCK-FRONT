52878a6585c6d94e166a3cc5c386f9118a8d3763
Deveventlab <rolanddevjunior@gmail.com>
2026-02-09 09:19:05 +0100

    update

--- src/app/list-modules/parametrage/article/article.component.ts
+++ src/app/list-modules/parametrage/article/article.component.ts
@@ -64,6 +64,7 @@ export class ArticleComponent implements OnInit {
       famille_id: ["", [Validators.required]],
       code: ["", [Validators.required]],
       designation: ["", [Validators.required]],
+      seuil_alerte: [0, [Validators.required]],
       description: ["", []],
     });
     this.editArticleForm = this.formBuilder.group({
@@ -71,6 +72,7 @@ export class ArticleComponent implements OnInit {
       famille_id: ["", [Validators.required]],
       code: ["", [Validators.required]],
       designation: ["", [Validators.required]],
+      seuil_alerte: [0, [Validators.required]],
       description: ["", []],
     });
     this.deleteArticleForm = this.formBuilder.group({
@@ -160,6 +162,7 @@ export class ArticleComponent implements OnInit {
       famille_id: row.famille_id,
       description: row.description,
       designation: row.designation,
+      seuil_alerte: row.seuil_alerte
     })
   }
 
